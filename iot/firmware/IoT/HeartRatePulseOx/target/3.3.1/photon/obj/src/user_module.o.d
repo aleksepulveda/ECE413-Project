ff95f7a1b37c3e9388d25ab8625bf569db55371e
Darryl Mercado <53824957+PringleCan@users.noreply.github.com>
2025-11-23 12:54:57 -0700

    MilestoneDay Not working

---
+++ iot/firmware/IoT/HeartRatePulseOx/target/3.3.1/photon/obj/src/user_module.o.d
@@ -0,0 +1,3 @@
+c\:/Users/darry/OneDrive/Documents/GitHub/ECE413-Project/iot/firmware/IoT/HeartRatePulseOx/target/3.3.1/photon//obj/src/user_module.o: \
+ src/user_module.c ../../../dynalib/inc/dynalib.h
+../../../dynalib/inc/dynalib.h:
---
+++ iot/firmware/IoT/HeartRatePulseOx/target/6.2.1/p2/obj/src/user_module.o.d
@@ -0,0 +1,7 @@
+c\:/Users/darry/OneDrive/Documents/GitHub/ECE413-Project/iot/firmware/IoT/HeartRatePulseOx/target/6.2.1/p2//obj/./src/user_module.o: \
+ src/user_module.c \
+ ../../../modules/shared/rtl872x/inc/user-part/user_module.inc \
+ ../../../dynalib/inc/dynalib.h ../../../hal/shared/platforms.h
+../../../modules/shared/rtl872x/inc/user-part/user_module.inc:
+../../../dynalib/inc/dynalib.h:
+../../../hal/shared/platforms.h:
